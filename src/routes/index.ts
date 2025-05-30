346600ddbce39d7e57a31553e84c0ff07ec2eb37
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-30 18:20:24 -0300

    feat: add sessions routes to main routing setup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { userRoutes } from "./users-routes";
+import { sessionsRoutes } from "./sessions-routes";
 
 const routes = Router()
 
+// Rotas públicas
 routes.use("/users", userRoutes)
+routes.use("/sessions", sessionsRoutes)
 
 export { routes }
