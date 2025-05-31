e957d2ee68c119813ba8acb079657d32d9d68598
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-31 15:28:40 -0300

    feat: add private routes for admin with authentication middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { userRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { adminRoutes } from "./admin-routes";
 
 const routes = Router()
 
 // Rotas públicas
 routes.use("/users", userRoutes)
 routes.use("/sessions", sessionsRoutes)
+
+// Rotas privadas
+routes.use(ensureAuthenticated)
+routes.use("/admin", adminRoutes)
 
 export { routes }
