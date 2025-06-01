50d6c3b91b0605637d1a4ed6c5ef552ef199d5c5
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-31 23:26:49 -0300

    feat: implement user management routes with authentication and authorization

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,9 +1,27 @@
 import { Router } from "express";
 import { UsersControllers } from "@/controllers/users-controller";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const usersController = new UsersControllers()
 const userRoutes = Router()
 
 userRoutes.post("/", usersController.create)
+
+userRoutes.get("/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["Admin"]),
+  usersController.index
+)
+userRoutes.patch("/:userId",
+  ensureAuthenticated,
+  verifyUserAuthorization(["Admin"]),
+  usersController.update
+)
+userRoutes.delete("/:id",
+  ensureAuthenticated,
+  verifyUserAuthorization(["Admin"]),
+  usersController.remove
+)
 
 export { userRoutes }
