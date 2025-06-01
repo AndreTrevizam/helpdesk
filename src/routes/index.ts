db124f273904abe6437f0929c081245cd688a2f6
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-31 23:26:54 -0300

    feat: remove Admin routes from the main routing file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,6 @@ import { sessionsRoutes } from "./sessions-routes";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { techniciansRoutes } from "./technicians-routes";
 import { servicesRoutes } from "./services-routes";
-import { adminsRoutes } from "./admin-routes";
 
 const routes = Router()
 
@@ -16,6 +15,5 @@ routes.use("/sessions", sessionsRoutes)
 routes.use(ensureAuthenticated)
 routes.use("/technicians", techniciansRoutes)
 routes.use("/services", servicesRoutes)
-routes.use("/admin", adminsRoutes)
 
 export { routes }
