8409a4caa9ae27a665f9ab216394876cfeca5e56
AndreTrevizam <andretrevizam264@gmail.com>
2025-06-03 19:05:30 -0300

    test: add user service tests for user creation and validation

---
+++ src/tests/users-service.test.ts
@@ -0,0 +1,45 @@
+import request from "supertest"
+import { app } from "@/app"
+import { prisma } from "@/database/prisma"
+
+describe("UserService", () => {
+  let user_id: string
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } })
+  })
+
+  it("Should create a new user successfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test user",
+      email: "test@test.com",
+      password: "testtest",
+    })
+
+    expect(response.status).toBe(201)
+
+    user_id = response.body.id
+  })
+
+  it("Should throw an error if user with same email already exists", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Duplicated User",
+      email: "test@test.com",
+      password: "testtest",
+    })
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe("E-mail já cadastrado!")
+  })
+
+  it("Should throw an error if email is invalid", async () => {
+    const response = await request(app).post("/users").send({
+      name: "User",
+      email: "invalid-email",
+      password: "testtest",
+    })
+
+    expect(response.status).toBe(400)
+    expect(response.body.message).toBe("Erro de validação!")
+  })
+})
