7b7e0778ecab940f7baef675585865700b5ba779
AndreTrevizam <andretrevizam264@gmail.com>
2025-06-05 20:25:13 -0300

    test: add integration tests for Admins Controller

---
+++ src/tests/admins-controller.test.ts
@@ -0,0 +1,69 @@
+import request from "supertest"
+import { app } from "@/app"
+import { prisma } from "@/database/prisma"
+
+describe("Admins Controller", () => {
+  let admin_token: string
+  let technicians_id: string[] = []
+
+  beforeAll(async () => {
+    await request(app).post("/users").send({
+      name: "admin test",
+      email: "test@admin.com",
+      password: "123456",
+      role: "Admin"
+    })
+
+    const adminResponse = await request(app).post("/sessions").send({
+      email: "test@admin.com",
+      password: "123456"
+    })
+
+    admin_token = adminResponse.body.token
+    console.log(admin_token)
+  })
+
+  it("Should create a new Technician", async () => {
+    const response = await request(app)
+      .post("/admins")
+      .set("Authorization", `Bearer ${admin_token}`)
+      .send({
+        name: "Technician Teste",
+        email: "technician@test.com",
+        password: "123456",
+        availableTimes: ["10:00", "11:00"],
+      })
+
+    console.log(response.status)
+    console.log(response.text)
+    expect(response.status).toBe(201)
+  })
+
+  it("Should list all Technicians", async () => {
+    const response = await request(app).get("/admins").set("Authorization", `Bearer ${admin_token}`)
+
+    expect(response.status).toBe(200)
+    expect(response.body.technicians).toBeInstanceOf(Array)
+    expect(response.body.pagination).toMatchObject({
+      page: 1,
+      perPage: 5,
+      totalRecords: expect.any(Number),
+      totalPages: expect.any(Number),
+    })
+  })
+
+  afterAll(async () => {
+    await prisma.technician.deleteMany()
+    await prisma.user.deleteMany({
+      where: {
+        email: {
+          in: [
+            "test@admin.com",
+            "technician@test.com"
+          ]
+        }
+      }
+    })
+    await prisma.$disconnect
+  })
+})
